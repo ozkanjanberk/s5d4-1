b4cc387b86ea6d31f9d638b686e5e30765747143
ozkanjanberk <ozkanjanberk@gmail.com>
2024-03-22 09:18:51 +0300

    Task 1: 9 out of 12 completed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Aşağıdaki Fonksiyonu değiştirmeyin
 
 export async function ipAdresimiAl() {
   return await axios({
-    method: 'get',
-    url: 'https://apis.ergineer.com/ipadresim',
+    method: "get",
+    url: "https://apis.ergineer.com/ipadresim",
   }).then(function (response) {
     return response.data;
   });
@@ -27,7 +27,13 @@ export async function ipAdresimiAl() {
 */
 
 export async function getData() {
-  /* kodlar buraya */
+  const ip = await ipAdresimiAl();
+  const response = await axios
+    .get(`https://apis.ergineer.com/ipgeoapi/${ip}`)
+    .then((response) => {
+      return response.data;
+    });
+  return response;
 }
 
 /*
@@ -52,6 +58,31 @@ export async function getData() {
   </div>
 */
 
-export function cardOlustur(/* kodlar buraya */) {
-  /* kodlar buraya */
+export function cardOlustur(item) {
+  console.log(item);
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const image = document.createElement("img");
+  image.src = `https://flaglog.com/codes/standardized-rectangle-120px/TR.png`;
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+  const h3 = document.createElement("h3");
+  h3.classList.add("ip");
+  h3.textContent = item.sorgu;
+  const p1 = document.createElement("p");
+  p1.classList.add("ulke");
+  p1.textContent = item.ülkeKodu;
+  const p2 = document.createElement("p");
+  p2.textContent = `Enlem: ${item.enlem} Boylam: ${item.boylam}`;
+  const p3 = document.createElement("p");
+  p3.textContent = `${item.şehir}`;
+  const p4 = document.createElement("p");
+  p4.textContent = `Saat dilimi: ${item.saatdilimi}`;
+  const p5 = document.createElement("p");
+  p5.textContent = `Para birimi: ${item.parabirimi}`;
+  const p6 = document.createElement("p");
+  p6.textContent = `ISP: ${item.isp}`;
+  cardInfo.append(h3, p1, p2, p3, p4, p5, p6);
+  card.append(image, cardInfo);
+  return card;
 }
